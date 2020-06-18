e4303dde3dda97a90a3400d54ddb64e81ba9327e
daria <cula.lou@hotmail.de>
2020-06-18 17:21:00 +0200

    mean, std

--- Evaluation.py
+++ Evaluation.py
@@ -18,6 +18,7 @@ import os
 import re
 from openpyxl import Workbook, load_workbook
 from openpyxl.styles import Alignment, NamedStyle, Font
+import numpy as np
 
 
 def calculate_loss_and_accuracy(model, X_test, y_test):
@@ -64,8 +65,59 @@ def calculate_hausdorff_distance(gt_img_path, pred_img_path):
     return hd, avg_hd
 
 
+def calculate_mean(results):
+    # organize metrics of all patients
+    hd = []
+    avg_hd = []
+    dice = []
+    avg_ol = []
+    vs = []
+    for result in results:
+        hd.append(result[1])
+        avg_hd.append(result[2])
+        dice.append((result[3]))
+        avg_ol.append(result[4])
+        vs.append(result[5])
+
+    # calculate mean of all metrics
+    mean_hd = np.mean(hd)
+    mean_avg_hd = np.mean(avg_hd)
+    mean_dice = np.mean(dice)
+    mean_avg_ol = np.mean(avg_ol)
+    mean_vs = np.mean(vs)
+
+    mean = ["mean", mean_hd, mean_avg_hd, mean_dice, mean_avg_ol, mean_vs]
+    return mean
+
+
+def calculate_standard_dv(results):
+    # organize metrics of all patients
+    hd = []
+    avg_hd = []
+    dice = []
+    avg_ol = []
+    vs = []
+    for result in results:
+        hd.append(result[1])
+        avg_hd.append(result[2])
+        dice.append((result[3]))
+        avg_ol.append(result[4])
+        vs.append(result[5])
+
+    # calculate std of all metrics
+    std_hd = np.std(hd)
+    std_avg_hd = np.std(avg_hd)
+    std_dice = np.std(dice)
+    std_avg_ol = np.std(avg_ol)
+    std_vs = np.std(vs)
+
+    std = ["standard d", std_hd, std_avg_hd, std_dice, std_avg_ol, std_vs]
+    return std
+
+
 def evaluate_predictions(pred_path, gt_path):
     results = []
+    std = []
 
     dict_gt_paths = get_dict_of_paths(gt_path)
 
@@ -83,12 +135,17 @@ def evaluate_predictions(pred_path, gt_path):
 
         results.append([patient_no, hd, avg_hd, dice, avg_ol, vs])
 
-    return results
+    mean = calculate_mean(results)
+    std = calculate_standard_dv(results)
+
+    return results, mean, std
 
 
 '''
 https://realpython.com/openpyxl-excel-spreadsheets-python/
 '''
+
+
 def create_excel_sheet(organ):
     # create excel sheet
     wb = Workbook()
@@ -97,8 +154,8 @@ def create_excel_sheet(organ):
 
     # create headings and apply style
     headings_style = NamedStyle(
-        name = "daria",
-        font=Font(color='000000',bold=True),
+        name="daria",
+        font=Font(color='000000', bold=True),
         alignment=Alignment(horizontal='left')
     )
     headings_row = '1'
@@ -128,7 +185,7 @@ def fill_excel_sheet(save_path_results, save_path_y_test, organ):
     sheet = wb.active
 
     # get metrics
-    results = evaluate_predictions(save_path_results, save_path_y_test)
+    results, mean, std = evaluate_predictions(save_path_results, save_path_y_test)
 
     # write metrics into excel
     number_of_results = len(results)
@@ -137,12 +194,26 @@ def fill_excel_sheet(save_path_results, save_path_y_test, organ):
     start_col = 1
     for i in range(0, number_of_results):
         for j in range(0, number_of_metrics):
-            row = start_row + i # start a new row for every result
-            col = start_col + j # put metrcis into the right column
+            row = start_row + i  # start a new row for every result
+            col = start_col + j  # put metrcis into the right column
             # cut all values to 2 decimal places. Except patient number
             metric_value = "{:.2f}".format(results[i][j])
             if j == 0:
                 metric_value = "{}".format(results[i][j])
             sheet.cell(column=col, row=row, value=metric_value)
+
+    # write mean and std also into excel
+    mean_row = start_row + number_of_results
+    std_row = mean_row + 1
+    for i in range(0, len(mean)):
+        col = start_col + i
+        if i == 0:
+            mean_value = "{}".format(mean[i])
+            std_value = "{}".format(std[i])
+        else:
+            mean_value = "{:.2f}".format(mean[i])
+            std_value = "{:.2f}".format(std[i])
+        sheet.cell(column=col, row=mean_row, value=mean_value)
+        sheet.cell(column=col, row=std_row, value=std_value)
 
     wb.save("Evaluation {}.xlsx".format(organ))
