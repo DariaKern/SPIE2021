5c5d393a4c79915482b3d7b1e502f7a8b2b53c8f
daria <cula.lou@hotmail.de>
2020-07-09 19:37:21 +0200

    generate gt bb with sitk

--- RRF/test.py
+++ RRF/test.py
@@ -22,21 +22,25 @@ for file in os.scandir(GT_SEG_PATH):
 
     for organ in [150, 156, 157, 160, 170]:
         lsi_filter = sitk.LabelStatisticsImageFilter()
+
         lsi_filter.Execute(img_scan, img_seg)
         v = lsi_filter.GetBoundingBox(organ)
 
         # convert image coordinates to world
-        worldV = [worldOrig[0] + v[0] * worldSpacg[0],
-                  worldOrig[0] + v[1] * worldSpacg[0],
-                  worldOrig[1] + v[2] * worldSpacg[1],
-                  worldOrig[1] + v[3] * worldSpacg[1],
-                  worldOrig[2] + v[4] * worldSpacg[2],
-                  worldOrig[2] + v[5] * worldSpacg[2]]
+        worldV = [((worldOrig[0] + v[0]) * worldSpacg[0])-1,
+                  ((worldOrig[0] + v[1]) * worldSpacg[0])+1,
+                  ((worldOrig[1] + v[2]) * worldSpacg[1])-1,
+                  ((worldOrig[1] + v[3]) * worldSpacg[1])+1,
+                  ((worldOrig[2] + v[4]) * worldSpacg[2])-1,
+                  ((worldOrig[2] + v[5]) * worldSpacg[2])+1]
+
+        if(patient_number == 7):
+            print(worldV)
         bb = vtk.vtkCubeSource()
         bb.SetBounds(worldV)
         bb.Update()
 
-        print(bb)
+        #print(bb)
 
         bb_name = "{}_{}_bb.vtk".format(patient_number, organ)
         save_path = "{}{}".format(GT_BB_PATH, bb_name)
